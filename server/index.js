87d1e4c9c93c646a47bfa8f3534b155b928e00e3
Rahil Ahmed <rahilahmed1720@gmail.com>
2024-12-02 22:26:56 +0530

    add index file

---
+++ server/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const authRoutes = require("./routes/auth");
+const database = require("./config/database");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const { cloudinaryConnect } = require("./config/cloudinary");
+const fileUpload = require("express-fileupload");
+const dotenv = require("dotenv");
+
+const app = express();
+
+dotenv.config();
+const port = process.env.PORT || 4000;
+
+database.connect();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp",
+  })
+);
+
+cloudinaryConnect();
+
+// Routes
+app.use("/api/v1/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  return res.json({
+    success: true,
+    message: "Your server is up and running....",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App is running at ${port}`);
+});
