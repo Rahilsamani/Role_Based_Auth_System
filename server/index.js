d0d7b2da3763f5b5cf65b2a6ab13f4aecc1d2a29
Rahil Ahmed <rahilahmed1720@gmail.com>
2024-12-03 23:05:32 +0530

    add frontend url

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://rbacauthsystem.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true,
   })
